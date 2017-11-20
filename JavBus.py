cb5c64a54035b0a9cbb57ba20a6f0d68852e23a4
fts3232 <550610606@qq.com>
2017-11-20 18:04:02 +0800

    s

--- JavBus.py
+++ JavBus.py
@@ -1,19 +1,49 @@
-import random;
-import urllib;
-import subprocess;
-import re;
-class JavBus:
+from bs4 import BeautifulSoup
+from Visitor import Visitor
+
+
+class JavBus(Visitor):
     'JavBus'
-    publishPage;
-    url;
-    def __init(self):
-        self.publishPage = 'https://announce.javbus2.pw/website.php';
-        self.url = '';
-    def getUrls(self):
-        pass;
-    def getFastUrl(self,urls):
-        pass;
-    def getMovieList(self,body):
-        pass;
+    publish_page = 'https://announce.javbus2.pw/website.php'
+    url = None
+    __visitor = Visitor()
+
+    def __init__(self):
+        pass
+
+    def get_urls(self, body):
+        soup = BeautifulSoup(body, "html.parser")
+        tags = soup.find_all('a')
+        urls = []
+        for tag in tags:
+            urls.append(tag['href'])
+        return urls
+
+    def get_fast_url(self, urls):
+        time = 0
+        fast = None
+        for url in urls:
+            ret = self.__visitor.ping(url)
+            if(ret['alive'] is True and (ret['time'] <= time or fast is None)):
+                fast = ret
+                time = ret['time']
+        return fast
+
+    def get_movie_list(self, body):
+        soup = BeautifulSoup(body, "html.parser")
+        print(soup)
+        tags = soup.find_all('a', class_="movie-box")
+        movie_list = []
+        for tag in tags:
+            movie_list.append(tag['href'])
+        return movie_list
+
     def run(self):
-        pass;
+        ret = self.__visitor.visit(self.publish_page)
+        urls = self.get_urls(ret)
+        fast = self.get_fast_url(urls)
+        ret = self.__visitor.visit(fast['url'])
+        self.get_movie_list(ret)
+
+
+JavBus().run()
--- Visitor.py
+++ Visitor.py
@@ -1,55 +1,64 @@
-import random;
-import urllib;
-import subprocess;
-import re;
-import urllib.request;
-import socket;
+import random
+import urllib
+import subprocess
+import urllib.request
+import socket
+
+
 class Visitor:
     '访问类'
-    def __init(self):
-        pass;
-    def createIP(self):
-        return "{}.{}.{}.{}".format(random.randint(1,254),random.randint(1,254),random.randint(1,254),random.randint(1,254)); 
-    def getHost(self,url):
+
+    def __init__(self):
+        pass
+
+    def create_ip(self):
+        return "{}.{}.{}.{}".format(random.randint(1, 254), random.randint(1, 254), random.randint(1, 254), random.randint(1, 254))
+
+    def get_host(self, url):
         return urllib.parse.urlparse(url).netloc
-    def getHeaders(self,options={}):
+
+    def get_headers(self, options={}):
         headers = {
             'User-Agent': 'User-Agent:Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-            'X-Forwarded-For':self.createIP() ,
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding':'*',
-            'Accept-Language':'zh-CN,zh;q=0.8',
-            'Cache-Control':'no-cache',
-            'Connection':'keep-alive',
-            'Cache-Control':'no-cache',
-            'Pragma':'no-cache',
-            'Upgrade-Insecure-Requests':'1'
-        };
+            'X-Forwarded-For': self.create_ip(),
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': '*',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Upgrade-Insecure-Requests': '1'
+        }
         if('host' in options):
-            headers['host'] = options['host'];
+            headers['host'] = options['host']
         if('referer' in options):
-            headers['referer'] = options['referer'];
-        return headers;
-    def visit(self,url,options={}):
+            headers['referer'] = options['referer']
+        return headers
+
+    def visit(self, url, options={}):
         try:
             socket.setdefaulttimeout(10)
-            request = urllib.request.Request(url,{},self.getHeaders(options));
-            response = urllib.request.urlopen(request);
-            print(response.read().decode('utf-8'));
-            response.close();
+            request = urllib.request.Request(url, {}, self.get_headers(options))
+            response = urllib.request.urlopen(request)
+            ret = response.read().decode('utf-8', 'ignore')
+            response.close()
+            print('访问' + url + '成功')
+            return ret
         except Exception as e:
-            print(e);
-        pass; 
-    def ping(self,url):
+            print(e)
+
+    def ping(self, url):
         try:
-            ret = subprocess.Popen(["ping.exe", url],
+            host = self.get_host(url)
+            ret = subprocess.Popen(["ping.exe", host],
                                    shell=True,
                                    stdout=subprocess.PIPE)
-            ret = str(ret.stdout.read());
-            alive = True;
-            time = ret[ret.rindex('=')+2:ret.rindex('ms')]
+            ret = str(ret.stdout.read())
+            alive = True
+            time = ret[ret.rindex('=') + 2:ret.rindex('ms')]
         except Exception as e:
-            alive = False;
-            time = 'unkown';
-        return {'time':time,'alive':alive};
-a = Visitor().visit('https://announce.javbus2.pw/website.php');
+            alive = False
+            time = 'unkown'
+        print('ping ' + host + '完成，平均时间为' + time)
+        return {'url': url, 'time': int(time), 'alive': alive}
