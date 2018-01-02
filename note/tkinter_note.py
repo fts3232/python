24da572f91a9a2f7c55e6251281ec6ce0715d0bc
fts3232 <550610606@qq.com>
2018-01-02 23:25:10 +0800

    v

---
+++ note/tkinter_note.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import tkinter
+import tkinter.messagebox
+# PyQt5
+#
+# 安装：自带模块
+#t
+frame = tkinter.Tk()
+text = tkinter.Text(frame,height = 1,width = 30)  
+text.grid(row = 0,column = 1)
+frame.mainloop()
+
