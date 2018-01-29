71d8f63b4b2e9b52b54d7b98c8d50fbe70e707db
fts3232 <550610606@qq.com>
2018-01-29 23:58:27 +0800

    v

--- note/dlib_note.py
+++ note/dlib_note.py
@@ -8,17 +8,11 @@ from pylab import *
 # 安装：pip install dlib
 # 需要安装cmake和boost
 img = cv2.imread('t.jpg')
-cv2.rectangle(img,(154,94),(333,273),(55,255,155),2)
-#cv2.imshow('image',img)
-figure()
-# ...省略所有的冒号来用省略号,-1代表最后一个维度 ::-1 翻转
-imshow(img[...,-1::-1])
-show()
-exit()
 open_img = Image.open('t.jpg')
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 detector = dlib.get_frontal_face_detector()
 dets = detector(img)
+figure()
 print("人脸数：", len(dets))
 # 输出人脸矩形的四个坐标点
 for i, d in enumerate(dets):
@@ -27,6 +21,14 @@ for i, d in enumerate(dets):
           "right:", d.right(),
           "top:", d.top(),
           "bottom:", d.bottom())
-    # shape = predictor(img, d)
-    cropped_img = open_img.crop((d.left(), d.top(), d.right(), d.bottom()))
-    cropped_img = cropped_img.resize((96, 96))
+    shape = predictor(img, d)
+    for i in range(68):
+        pt = shape.part(i)
+        plot(pt.x, pt.y, 'r.')
+    # cropped_img = open_img.crop((d.left(), d.top(), d.right(), d.bottom()))
+    # cropped_img = cropped_img.resize((96, 96))
+    # cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (55, 255, 155), 2)
+
+# ...省略所有的冒号来用省略号,-1代表最后一个维度 ::-1 翻转
+imshow(img[..., -1::-1])
+show()
